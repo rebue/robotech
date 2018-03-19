f0da14260cff22bf724f0bb07ba2bbf91c308a7e
zbz <zbz@zbz-nb1>
2018-03-19 13:02:53 +0800

    1.0.5

---
+++ robotech-bll/src/main/java/rebue/robotech/svc/impl/MybatisBaseSvcImpl.java
@@ -0,0 +1,114 @@
+package rebue.robotech.svc.impl;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
+import rebue.robotech.mapper.MybatisBaseMapper;
+import rebue.robotech.svc.MybatisBaseSvc;
+import rebue.wheel.idworker.IdWorker3;
+
+@Service
+/**
+ * <pre>
+ * 在单独使用不带任何参数 的 @Transactional 注释时，
+ * propagation(传播模式)=REQUIRED，readOnly=false，
+ * isolation(事务隔离级别)=READ_COMMITTED，
+ * 而且事务不会针对受控异常（checked exception）回滚。
+ * 注意：
+ * 一般是查询的数据库操作，默认设置readOnly=true, propagation=Propagation.SUPPORTS
+ * 而涉及到增删改的数据库操作的方法，要设置 readOnly=false, propagation=Propagation.REQUIRED
+ * </pre>
+ */
+@Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
+public class MybatisBaseSvcImpl<MO, ID, MAPPER extends MybatisBaseMapper<MO, ID>> implements MybatisBaseSvc<MO, ID> {
+
+    private final static Logger _log = LoggerFactory.getLogger(MybatisBaseSvcImpl.class);
+
+    @Autowired
+    protected MAPPER            _mapper;
+
+    @Value("${appid:0}")
+    private int                 _appid;
+
+    protected IdWorker3         _idWorker;
+
+    @PostConstruct
+    public void init() {
+        _idWorker = new IdWorker3(_appid);
+    }
+
+    @Override
+    public void test() {
+
+    }
+
+    @Override
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+    public int add(MO mo) {
+        _log.info("add: {}", mo);
+        return _mapper.insertSelective(mo);
+    }
+
+    @Override
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+    public int modify(MO mo) {
+        _log.info("modify: {}", mo);
+        return _mapper.updateByPrimaryKeySelective(mo);
+    }
+
+    @Override
+    @Transactional(readOnly = false, propagation = Propagation.REQUIRED)
+    public int del(ID id) {
+        _log.info("del: {}", id);
+        return _mapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public List<MO> listAll() {
+        _log.info("listAll");
+        return _mapper.selectAll();
+    }
+
+    @Override
+    public List<MO> list(MO mo) {
+        _log.info("list: {}", mo);
+        return _mapper.selectSelective(mo);
+    }
+
+    @Override
+    public PageInfo<MO> list(MO qo, int pageNum, int pageSize) {
+        _log.info("list: qo-{}; pageNum-{}; pageSize-{}", qo, pageNum, pageSize);
+        return PageHelper.startPage(pageNum, pageSize).doSelectPageInfo(() -> _mapper.selectSelective(qo));
+    }
+
+    @Override
+    public MO getById(ID id) {
+        _log.info("getById: {}", id);
+        return _mapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public boolean existByPrimaryKey(ID id) {
+        _log.info("existByPrimaryKey: {}", id);
+        return _mapper.existByPrimaryKey(id);
+    }
+
+    @Override
+    public boolean existSelective(MO mo) {
+        _log.info("existSelective: {}", mo);
+        return _mapper.existSelective(mo);
+    }
+
+}
