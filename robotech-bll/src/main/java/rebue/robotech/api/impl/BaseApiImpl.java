aa8f958a44d8d53fcd37d3d98481284a57ee152f
zbz <nnzbz@qq.com>
2020-06-12 16:04:39 +0800

    * 优化代码

--- robotech-bll/src/main/java/rebue/robotech/api/impl/BaseApiImpl.java
+++ robotech-bll/src/main/java/rebue/robotech/api/impl/BaseApiImpl.java
@@ -9,14 +9,14 @@ import rebue.robotech.svc.BaseSvc;
 
 public abstract class BaseApiImpl<ID, MO, JO, SVC extends BaseSvc<ID, MO, JO>> implements BaseApi<ID, MO> {
     @Autowired // 这里不能用@Resource，否则启动会报 `required a single bean, but xxx were found` 的错误
-    protected SVC _svc;
+    protected SVC svc;
 
     /**
      * 添加
      */
     @Override
     public Ro add(final MO mo) {
-        return _svc.add(mo);
+        return svc.add(mo);
     }
 
     /**
@@ -24,7 +24,7 @@ public abstract class BaseApiImpl<ID, MO, JO, SVC extends BaseSvc<ID, MO, JO>> i
      */
     @Override
     public Ro modify(final MO mo) {
-        return _svc.modify(mo);
+        return svc.modify(mo);
     }
 
     /**
@@ -32,18 +32,18 @@ public abstract class BaseApiImpl<ID, MO, JO, SVC extends BaseSvc<ID, MO, JO>> i
      */
     @Override
     public Ro del(final ID id) {
-        return _svc.del(id);
+        return svc.del(id);
     }
 
     @Override
     public MO getById(final ID id) {
-        return _svc.getById(id);
+        return svc.getById(id);
     }
 
     @Override
     public PageRo<MO> list(final MO qo, final Integer pageNum, final Integer pageSize, final String orderBy,
             final Integer limitPageSize) {
-        return _svc.list(qo, pageNum, pageSize, orderBy, limitPageSize);
+        return svc.list(qo, pageNum, pageSize, orderBy, limitPageSize);
     }
 
 }
