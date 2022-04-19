5b1ede4e5279bee7c51445ac3beeb9a939fcd043
zbz <nnzbz@21cn.com>
2022-04-19 12:56:26 +0800

    * 优化Ro类

--- robotech-api/src/main/java/rebue/robotech/ro/Ro.java
+++ robotech-api/src/main/java/rebue/robotech/ro/Ro.java
@@ -2,15 +2,13 @@ package rebue.robotech.ro;
 
 import java.io.Serializable;
 
-import org.springframework.lang.NonNull;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import rebue.robotech.dic.ResultDic;
 
 /**
@@ -20,7 +18,6 @@ import rebue.robotech.dic.ResultDic;
  */
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor // 不知道@Data中默认包含的@RequiredArgsConstructor为何没起效
 @AllArgsConstructor
 @JsonInclude(Include.NON_NULL)
 public class Ro<T> implements Serializable {
@@ -29,13 +26,11 @@ public class Ro<T> implements Serializable {
     /**
      * 返回结果的类型
      */
-    @NonNull
     private ResultDic         result;
 
     /**
      * 返回结果的信息
      */
-    @NonNull
     private String            msg;
 
     /**
@@ -44,16 +39,26 @@ public class Ro<T> implements Serializable {
     private String            detail;
 
     /**
+     * 附加的内容
+     * (如果前面的属性已经能够满足需求，可不需要附加的内容，设为null或不设置即可)
+     */
+    private T                 extra;
+
+    /**
      * 返回结果的自定义编码
      * (如果通过result已经能够满足需求，可不需要自定义编码，设为null或不设置即可)
      */
     private String            code;
 
-    /**
-     * 附加的内容
-     * (如果前面的属性已经能够满足需求，可不需要附加的内容，设为null或不设置即可)
-     */
-    private T                 extra;
+    public Ro(final ResultDic result, final String msg) {
+        this.result = result;
+        this.msg    = msg;
+    }
+
+    public Ro(final ResultDic result, final T extra) {
+        this.result = result;
+        this.extra  = extra;
+    }
 
     public Ro(final ResultDic result, final String msg, final String detail) {
         this.result = result;
@@ -74,21 +79,65 @@ public class Ro<T> implements Serializable {
         this.extra  = extra;
     }
 
+    public Ro(final ResultDic result, final String msg, final String detail, final T extra) {
+        this.result = result;
+        this.msg    = msg;
+        this.detail = detail;
+        this.extra  = extra;
+    }
+
+    public static Ro<?> newSuccess(final String msg) {
+        return new Ro<>(ResultDic.SUCCESS, msg);
+    }
+
+    public static <T> Ro<T> newSuccess(final T extra) {
+        return new Ro<>(ResultDic.SUCCESS, extra);
+    }
+
+    public static <T> Ro<T> newSuccess(final String msg, final T extra) {
+        return new Ro<>(ResultDic.SUCCESS, msg, extra);
+    }
+
+    public static Ro<?> newFail(final String msg) {
+        return new Ro<>(ResultDic.FAIL, msg);
+    }
+
+    public static Ro<?> newFail(final String msg, final String detail) {
+        return new Ro<>(ResultDic.FAIL, msg, detail);
+    }
+
+    public static <T> Ro<T> newFail(final T extra) {
+        return new Ro<>(ResultDic.FAIL, extra);
+    }
+
+    public static <T> Ro<T> newFail(final String msg, final T extra) {
+        return new Ro<>(ResultDic.FAIL, msg, extra);
+    }
+
+    public static Ro<?> newWarn(final String msg) {
+        return new Ro<>(ResultDic.WARN, msg);
+    }
+
+    public static Ro<?> newWarn(final String msg, final String detail) {
+        return new Ro<>(ResultDic.WARN, msg, detail);
+    }
+
+    public static <T> Ro<T> newWarn(final T extra) {
+        return new Ro<>(ResultDic.WARN, extra);
+    }
+
+    public static <T> Ro<T> newWarn(final String msg, final T extra) {
+        return new Ro<>(ResultDic.WARN, msg, extra);
+    }
+
     /**
      * 判断是否成功
-     * 
+     *
      * @return 是否成功
      */
+    @JsonIgnore
     public boolean isSuccess() {
         return ResultDic.SUCCESS.equals(result);
-    }
-
-    public static <T> Ro<T> success(final T extra) {
-        return new Ro<>(ResultDic.SUCCESS, "", extra);
-    }
-
-    public static <T> Ro<T> fail(final String msg) {
-        return new Ro<>(ResultDic.FAIL, msg, null);
     }
 
 }
