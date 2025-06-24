16ee2cc3573e6bc2f53ac59ad97d0ca62969cdf2
zbz <nnzbz@qq.com>
2025-06-24 09:09:06 +0800

    * wheel版本升级到3.5.52

--- robotech-api/src/main/java/rebue/robotech/cst/CommonCst.java
+++ robotech-api/src/main/java/rebue/robotech/cst/CommonCst.java
@@ -5,7 +5,11 @@ package rebue.robotech.cst;
  */
 public interface CommonCst {
     /**
-     * 用户ID_系统(如果是系统的操作，比如定时器，请用此 ID 作为用户 ID)
+     * 用户ID_系统(如果是系统的操作，比如定时器、采集器，请用此 ID 作为用户 ID)
      */
-    Long USER_ID_SYS = 1L;
+    Long USER_ID_SYS         = 0L;
+    /**
+     * 用户ID_三方(如果是第三方调用API，请用此 ID 作为用户 ID)
+     */
+    Long USER_ID_THIRD_PARTY = 1L;
 }
